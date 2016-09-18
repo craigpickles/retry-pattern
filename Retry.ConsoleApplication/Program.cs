36e0f1d48514f1a6a81fc1b4c61ddb432243ed72
Craig Pickles <cpickles84@gmail.com>
2016-09-18 11:00:49 +0100

    Initial commit

---
+++ Retry.ConsoleApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace Retry.ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new WebTimeoutRetry().ExecuteAction(async () =>
+            {
+                using (var client = new HttpClient())
+                {
+                    await client.GetAsync("http://some-website.com");
+                }
+            });
+        }
+
+        public class WebTimeoutRetry : Retry
+        {
+            public override bool IsTemporaryException(Exception ex)
+            {
+                var webException = ex as WebException;
+                if (webException?.Status == WebExceptionStatus.Timeout)
+                    return true;
+
+                return false;
+            }
+        }
+    }
+}
---
+++ Retry.ConsoleApplication/Retry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Retry.ConsoleApplication
+{
+    public abstract class Retry
+    {
+        public int NumberOfRetries { get; set; }
+
+        public void ExecuteAction(Action action)
+        {
+            int currentRetry = 0;
+
+            for (;;)
+            {
+                try
+                {
+                    action();
+                }
+                catch (Exception ex)
+                {
+                    currentRetry++;
+
+                    if (currentRetry > NumberOfRetries || !IsTemporaryException(ex))
+                        throw;
+                }
+            }
+        }
+
+        public abstract bool IsTemporaryException(Exception ex);
+    }
+}
